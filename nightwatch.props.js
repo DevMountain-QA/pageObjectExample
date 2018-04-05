2a153285efe5334108d3476b0eb2aef3b642fc6e
AJ Larson <aj.larson@devmounta.in>
2018-04-05 11:33:58 -0600

    updated

--- nightwatch.props.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    resourcePath: "/Users/ajlarson/src/testing-resources/selenium-resources/",
-    seleniumServer: "selenium-server-standalone-3.11.0.jar",
-    chromedriver: "chromedriver"
- }
-
